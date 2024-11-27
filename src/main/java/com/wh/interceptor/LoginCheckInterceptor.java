bf9f990764150a3a15fdc340fcb891737026be2b
lewisFinch <defalt404@163.com>
2024-11-27 08:02:59 -0500

    Interceptor implemented

--- src/main/java/com/wh/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/wh/interceptor/LoginCheckInterceptor.java
@@ -1,27 +1,62 @@
 package com.wh.interceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wh.pojo.Result;
+import com.wh.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @Component
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+
+        String url = request.getRequestURL().toString();
+        log.info("Request URL : " + url);
+
+        String jwt = request.getHeader("token");
+
+        if(!StringUtils.hasLength(jwt)){
+            log.info("JWT is empty");
+            Result error = Result.error("NOT_LOGIN");
+            ObjectMapper objectMapper = new ObjectMapper();
+            String notLogin = objectMapper.writeValueAsString(error);
+            response.getWriter().write(notLogin);
+            return false;
+        }
+
+        try{
+            JwtUtils.parseJWT(jwt);
+        } catch (Exception e){
+            e.printStackTrace();
+            log.info("JWT parse error");
+            Result error = Result.error("NOT_LOGIN");
+            ObjectMapper objectMapper = new ObjectMapper();
+            String notLogin = objectMapper.writeValueAsString(error);
+            response.getWriter().write(notLogin);
+            return false;
+        }
+
+        log.info("JWT parse success");
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        System.out.println("postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        System.out.println("afterCompletion");
     }
 }
